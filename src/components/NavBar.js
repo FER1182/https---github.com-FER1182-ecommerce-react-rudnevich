98505d5c56da6c7ffa311d51f474914dacb621a4
ferni232 <fernandorudnevich.inedita@gmail.com>
2022-01-30 22:29:36 -0300

    agregue navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,32 @@
 import React from 'react';
 
 function NavBar() {
-  return <div>
-      esta es la barra
+  return 
+  <div>
+      {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a classNameclass="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+             <ul className="navbar-nav">
+                <li classNameclass="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Features</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Pricing</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link disabled">Disabled</a>
+                </li>
+              </ul>
+          </div>
+       </div>
+     </nav> */}
   </div>;
 }
 
