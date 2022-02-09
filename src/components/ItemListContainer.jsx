33e38660fd8d653883d474bacd257eef1012352e
ferni232 <fernandorudnevich.inedita@gmail.com>
2022-02-08 21:15:42 -0300

    itemcontainer

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
-const ItemListContainer = ({greeting}) => {
-  return <div>
-      <h1>{greeting}</h1>
-  </div>;
+const ItemListContainer = ({ greeting }) => {
+
+    const data = [{ id: "tr1000", name: "camisa", descripcion: "camisa con tachas", stock: 10 }];
+
+    const tarea = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(data)
+        }, 3000);
+
+    })
+
+    tarea.then((result) => {
+        console.log(result);
+    }).catch(err => {
+        console.log(err);
+    })
+
+    console.log(tarea);
+
+  
+
+
+    return <div>
+
+        <h1>{greeting}</h1>
+    </div>;
 };
 
 export default ItemListContainer;
