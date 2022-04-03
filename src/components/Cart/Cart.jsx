0a1d79e4c1ab83b3188da2807b24fda949fb3908
ferni232 <fernandorudnevich.inedita@gmail.com>
2022-04-03 15:16:47 -0300

    cambios boton vaciar carrito

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,45 +6,58 @@ import { CarritoContext } from '../Context/CarritoProvedor'
 
 
 const Cart = () => {
-  const { totalCarrito,carrito, eliminarDelCarrito } = useContext(CarritoContext);
+  const { totalCarrito, carrito, eliminarDelCarrito, vaciarCarrito } = useContext(CarritoContext);
 
- 
+
   return (
     <>
       {carrito.length > 0 ? (
-         
-        <div className="w-100 d-flex">
-          <div className="container col-3">
-          <Checkout carrito={carrito} />
-          </div>
-          
-          <div className="container col-6">
-            
-            {carrito.map((item) =>
-              <div className="card mb-3" style={{ width: "380px" }} key={item.itemDetalle.id}>
-                <div className="row g-0">
-                
-                  <div className="col-md-4">
-                    <img src={item.itemDetalle.img} className="img-fluid rounded-start" alt="..." />
-                  </div>
-                  <div className="col-md-4 align-middle">
+        <div className="container">
+          <div className="w-100 d-flex">
 
-                    <h5 className="align-middle ">art.{item.itemDetalle.modelo}</h5>
-                    <h5 className="card-description">$ {item.itemDetalle.precio}</h5>
-                  </div>
-                  <div className="col-md-4">
+            <div className="container col-6">
+              <Checkout carrito={carrito} />
+            </div>
 
-                    <h5 className="card-description"> cant. {item.cantidad}</h5>
-                    <h2 className="card-description"> subtotal $ {item.subTotal}</h2>
-                 
+            <div className="container col-6">
+
+              {carrito.map((item) =>
+                <div className="card mb-3" style={{ width: "380px" }} key={item.itemDetalle.id}>
+                  <div className="row g-0">
+
+                    <div className="col-md-4">
+                      <img src={item.itemDetalle.img} className="img-fluid rounded-start" alt="..." />
+                    </div>
+                    <div className="col-md-4 align-middle">
+
+                      <h5 className="align-middle ">art.{item.itemDetalle.modelo}</h5>
+                      <h5 className="card-description">$ {item.itemDetalle.precio}</h5>
+                    </div>
+                    <div className="col-md-4">
+
+                      <h5 className="card-description"> cant. {item.cantidad}</h5>
+                      <h2 className="card-description"> subtotal $ {item.subTotal}</h2>
+
+                    </div>
                   </div>
-                </div>
-                <button onClick={() => eliminarDelCarrito(item.itemDetalle.id)}>eliminar del carrito</button>
-              </div>)}
+                  <button onClick={() => eliminarDelCarrito(item.itemDetalle.id)}>eliminar del carrito</button>
+                </div>)}
               <h1>el total de la compra es $ {totalCarrito()}</h1>
+            </div>
+          </div>
+          <div className="container">
+            <div className="row align-items-end">
+              <button type="button" className="btn btn-outline-success btn-lg" onClick={vaciarCarrito}>Vaciar Carrito</button>
+            </div>
           </div>
         </div>
-      ) : <h1>Carrito Vacio</h1>}
+      ) : <div className="container">
+        <h1>Carrito Vacio</h1>
+          <div className="row align-items-end">
+            <a class="btn btn-outline-secondary btn-lg" href="/"  role="button">Agregar articulos al carrito</a>
+          </div>
+      </div>
+      }
     </>
 
 
--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState,useContext } from 'react';
+import React, {useContext } from 'react';
 import { CarritoContext } from './Context/CarritoProvedor';
 
 import Carrito from '../images/shopping-cart.png';
@@ -7,10 +7,9 @@ import { Link } from "react-router-dom";
 
 const CartWidget = () => {
 
-    const {carrito,vaciarCarrito} = useContext(CarritoContext);
+    const {carrito} = useContext(CarritoContext);
     return <div>
         <Link to={"/cart"} className='btn'>
-            <button onClick={vaciarCarrito}>Vaciar Carrito</button>
             <img src={Carrito} width={30} alt="..." />
             <p>Productos ({carrito.length})</p>
         </Link>
